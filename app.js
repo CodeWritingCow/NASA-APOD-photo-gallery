9e552f9e63ebecaa2c5df62c67143ab0b58b6a2f
Gary Pang <gary.pang@gmail.com>
2017-12-09 06:22:05 -0500

    Rewrite app in ES6
    Issue #1

--- app.js
+++ app.js
@@ -1,21 +1,19 @@
 /**
  * You must include the dependency on 'ngMaterial' 
  */
-var app = angular.module('photoApp', ['ngMaterial']);
+const app = angular.module('photoApp', ['ngMaterial']);
 
     // Get API token from server
-    app.factory('tokenFactory', function($http){
+    app.factory('tokenFactory', ($http) => {
       return {
-        async: function (){
-          return $http.get('/token');
-        }
+        async: () => $http.get('/token')
       };
     });
 
     // Create custom HTTP header name 'allow-access'
-    app.factory('httpRequestInterceptor', function(){
+    app.factory('httpRequestInterceptor', () =>{
       return {
-        request: function(config) {
+        request: (config) => {
           config.headers['x-custom-header-name'] = 'allow-access';
           return config;
         }
@@ -23,12 +21,10 @@ var app = angular.module('photoApp', ['ngMaterial']);
     });
 
     // Inject custom HTTP header name into http requests from Angular app
-    app.config(function($httpProvider){
-      $httpProvider.interceptors.push('httpRequestInterceptor');
-    });
+    app.config(($httpProvider) => $httpProvider.interceptors.push('httpRequestInterceptor'));
 
-    app.controller('photoCtrl', function($scope, $http, $sce, tokenFactory) {
-      tokenFactory.async().then(function(data){
+    app.controller('photoCtrl', ($scope, $http, $sce, tokenFactory) => {
+      tokenFactory.async().then((data) => {
         $scope.data = data;
         $scope.token = $scope.data.data.token;
 
@@ -47,10 +43,10 @@ var app = angular.module('photoApp', ['ngMaterial']);
       $scope.videoUrl;
       
       // Get photo data from NASA API
-      $scope.loadPhoto = function(url, token) {
+      $scope.loadPhoto = (url, token) => {
         $http.get(url, {
           params: {api_key: token}
-        }).then(function(response) {
+        }).then((response) => {
           $scope.photo = response.data;
 
           // If media is video, save url as videoUrl. (ex. APOD for 2017-02-01 is a video.)
@@ -63,7 +59,5 @@ var app = angular.module('photoApp', ['ngMaterial']);
       };
 
       // Convert date from '"YYYY-MM-DDT00:00:00.000Z"' to 'YYYY-MM-DD' per date parameter format for NASA API
-      $scope.convertDate = function(date) {
-        return date.toISOString().split('T')[0].toString();
-      };
+      $scope.convertDate = (date) => date.toISOString().split('T')[0].toString();
     });
--- server.js
+++ server.js
@@ -1,39 +1,36 @@
-var express = require('express'),
-	app = express(),
-	bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var port = process.env.PORT || 8080;
-var token = process.env.TOKEN || 'DEMO_KEY';
-
+const port = process.env.PORT || 8080;
+const token = process.env.TOKEN || 'DEMO_KEY';
 
 app.use(express.static(__dirname));
 
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
 	res.sendFile('index.html');
 });
 
 // If HTTP request is made to '/token' route without custom header name
 // Redirect to homepage. This prevents users from directly visiting the route
-app.use('/token', function(req, res, next){
+app.use('/token', (req, res, next) => {
 	if (!req.headers['x-custom-header-name']) {
 		res.redirect('/');
 	}
 	next();
 });
 
-app.get('/token', function(req, res) {
+app.get('/token', (req, res) => {
 	res.json({'token': token});
 });
 
 // If user tries to visit non-existent routes, redirect to hompage.
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
 	res.status(404).redirect('/');
 });
 
-app.listen(port, function(){
-	console.log('App is running on http://localhost:' + port);
-});
+app.listen(port, () => console.log(`App is running on port: ${port}`));
